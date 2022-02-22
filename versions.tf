76f966d0948c892a906611c26b978447ce4d086c
cytopia <cytopia@everythingcli.org>
2022-02-22 13:00:29 +0100

    Update Cloudflare provider

--- versions.tf
+++ versions.tf
@@ -3,7 +3,7 @@ terraform {
   required_providers {
     cloudflare = {
       source  = "cloudflare/cloudflare"
-      version = "~> 3.1.0"
+      version = "~> 3.9.1"
     }
   }
   required_version = ">= 0.13"
